58b58c18f27234e6afa5de847ec882c5437916cf
Pontus Freyhult <pontus.freyhult@nbis.se>
2020-10-14 00:09:40 +0200

    Support for helm test
    Test most services through "helm test" by providing false triggers for
    what comes from the outside in actual operation.

--- sda-svc/files/release-test.sh
+++ sda-svc/files/release-test.sh
@@ -3,16 +3,39 @@
 s=${PKI_PATH:-/certs}
 mkdir -p /tmp/c4gh
 
-dd if=/dev/urandom of=/tmp/testfile bs=1k count=16
 
+user=release-test-${RANDOM}
+
+tmpfile=$(tempfile)
+uploaded=${tmpfile##*/}.encrypted
+
+echo "Creating file $tmpfile"
+
+dd if=/dev/urandom of="$tmpfile" bs=1M count=16
+
+echo "Creating c4gh key"
 crypt4gh-keygen --sk /tmp/c4gh/key --pk /tmp/c4gh/key.pub --nocrypt -f
 
-crypt4gh encrypt --recipient_pk /testapp/c4gh.pub --sk /tmp/c4gh/key \
-      < /tmp/testfile \
-      > /tmp/testfile.encrypted
+echo "Encrypting file"
+crypt4gh encrypt --recipient_pk /release-test-app/c4gh.pub --sk /tmp/c4gh/key \
+      < "$tmpfile" \
+      > "${tmpfile}.encrypted"
 
-if [ "${STORAGE_TYPE}" = posix ]; then
-   cp /tmp/testfile.encrypted /posix/
+echo "Noting checksums"
+sha=$(sha256sum "${tmpfile}.encrypted" | cut -d' ' -f 1)
+decsha=$(sha256sum "${tmpfile}" | cut -d' ' -f 1)
+decmd=$(md5sum "${tmpfile}" | cut -d' ' -f 1)
+
+echo
+echo "Encrypted sha256: $sha"
+echo "Decrypted sha256: $decsha"
+echo "Decrypted md5: $decmd"
+echo
+
+if [ "${INBOX_STORAGE_TYPE}" = posix ]; then
+    echo "Copying file $tmpfile to posix inbox"
+    cp  "${tmpfile}.encrypted" /posix_inbox/
+    ls -la "/posix_inbox/${uploaded}"
 else
 
   cat - > /tmp/s3cmd.cfg <<EOF
@@ -34,22 +57,176 @@ EOF
   if [ -n "${INBOX_CACERT}" ]; then
      echo "ca_certs_file = ${INBOX_CACERT}" >> /tmp/s3cmd.cfg
   fi
-  s3cmd  --region="${INBOX_REGION}" -c /tmp/s3cmd.cfg put /tmp/testfile.encrypted "s3://${INBOX_BUCKET}"
+
+  echo "Copying to s3://${INBOX_BUCKET}/${uploaded}"
+  s3cmd  --region="${INBOX_REGION}" -c /tmp/s3cmd.cfg put  "${tmpfile}.encrypted" "s3://${INBOX_BUCKET}"
+
+  echo "Waiting for s3 consistency"
+  
+  sleep 60
+  s3cmd  --region="${INBOX_REGION}" -c /tmp/s3cmd.cfg ls "s3://${INBOX_BUCKET}/${uploaded}"
+
 fi
 
-
-sleep 60
 count=0
 
+export MQ_EXCHANGE=''
 
-MQ_EXCHANGE=''
-
-until python3 /testapp/trigger-ingest.py testuser /testfile.encrypted; do
+echo "Trying to trigger ingestion by message through mq"
+until python3 /release-test-app/trigger-ingest.py "$user" "$uploaded"; do
+    echo "MQ failed, will wait and retry"
     sleep 10
     count=$((count+1))
     if [ "$count" -gt 10 ]; then
-	break
+	echo "Could not send message after 100 s, bailing out"
+	exit 1
     fi
 done
-sleep 100
-exit 1
+
+# Fix db certs permissions
+PGSSL=/tmp/pgcerts
+mkdir -p "$PGSSL"
+
+PGSSLKEY=$PGSSL/postgresql.key
+PGSSLCERT=$PGSSL/postgresql.crt
+export PGSSL PGSSLKEY PGSSLCERT
+
+s=${PKI_VOLUME_PATH:-/certs}
+cp "$s/tester.ca.key" $PGSSL/postgresql.key
+cp "$s/tester.ca.crt" $PGSSL/postgresql.crt
+cp "$s/ca.crt" $PGSSL/root.crt
+
+chmod -R og-rw $PGSSL
+
+PGPASSWORD=${DB_PASSWORD}
+export PGPASSWORD
+
+echo "Will check in DB if file is archived"
+echo
+echo "Command used: psql -A -t  -h \"${DB_HOST}\" -U lega_in lega -c \
+	 \"select archive_path from local_ega.files where 
+          inbox_file_checksum='$sha' and
+          inbox_path='${uploaded}' and 
+          elixir_id='$user' and
+          status in ('ARCHIVED', 'COMPLETED', 'READY');\""
+echo
+# psql -h "${DB_HOST}" -U lega_in lega
+
+count=1
+until archivepath=$(psql -A -t  -h "${DB_HOST}" -U lega_in lega -c \
+	 "select archive_path from local_ega.files where 
+          inbox_file_checksum='$sha' and
+          inbox_path='${uploaded}' and 
+          elixir_id='$user' and
+          status in ('ARCHIVED', 'COMPLETED', 'READY');" | grep '.') || [ "$count" -ge 12 ]; do 
+    sleep 10;
+    count=$((count+1));
+done
+
+if [ -z "$archivepath" ]; then
+    echo "File did not show up in database after 2 minutes, giving up."
+    exit 1
+fi
+
+echo "File was archived as $archivepath"
+
+access=$(printf "EGAF%011d" "${RANDOM}${RANDOM}" )
+
+echo "Will send an accession id message"
+until python3 /release-test-app/accession.py "$user" "$archivepath" "$access" "$decsha" "$decmd"; do
+    sleep 10
+    count=$((count+1))
+    if [ "$count" -gt 10 ]; then
+	echo "Could not send message after 100 seconds, giving up."
+	exit 1
+    fi
+done
+
+
+echo "Will check database for ready file"
+echo 
+echo "Command used: psql -A -t  -h \"${DB_HOST}\" -U lega_in lega -c \
+	 \"select archive_path from local_ega.files where 
+          inbox_file_checksum='$sha' and
+          inbox_path='$uploaded' and 
+          stable_id='$access' and
+          elixir_id='$user' and
+          status='READY';\""
+echo
+
+count=0
+until readypath=$(psql -A -t  -h "${DB_HOST}" -U lega_in lega -c \
+	 "select archive_path from local_ega.files where 
+          inbox_file_checksum='$sha' and
+          inbox_path='$uploaded' and 
+          stable_id='$access' and
+          elixir_id='$user' and
+          status='READY';" | grep '.') || [ "$count" -ge 12 ]; do 
+    sleep 10;
+    echo "Not completed yet, will wait and retry"
+    count=$((count+1));
+done
+
+echo
+if [ -z "$readypath" ]; then
+    echo "File did not show up as ready in database after 2 minutes, giving up."
+    exit 1
+fi
+
+echo "File was ready - test succeeded"
+
+echo
+echo "Doing teardown"
+echo
+
+echo "Removing from database"
+echo
+psql -A -t  -h "${DB_HOST}" -U lega_in lega -c \
+	 "delete from local_ega.files where 
+          inbox_file_checksum='$sha' and
+          inbox_path='$uploaded' and 
+	  archive_path='$archivepath' and
+          stable_id='$access' and
+          elixir_id='$user' and
+          status='READY';"
+
+echo
+if [ "${INBOX_STORAGE_TYPE}" = posix ]; then
+    echo "Removing ${uploaded} from posix inbox if it's there"
+    rm -f "/posix_inbox/${uploaded}" 
+else
+  echo "Removing  s3://${INBOX_BUCKET}/${uploaded}"
+  s3cmd  --region="${INBOX_REGION}" -c /tmp/s3cmd.cfg del "s3://${INBOX_BUCKET}/${uploaded}"
+fi
+
+if [ "${ARCHIVE_STORAGE_TYPE}" = posix ]; then
+    echo "Removing $archivepath from posix archive if it's there"
+    rm -f "/posix_archive/$archivepath"
+else
+
+  cat - > /tmp/s3cmd.cfg <<EOF
+[default]
+access_key=${ARCHIVE_ACCESSKEY}
+secret_key=${ARCHIVE_SECRETKEY}
+check_ssl_certificate = True
+encoding = UTF-8
+encrypt = True
+guess_mime_type = True
+host_base = ${ARCHIVE_URL}
+host_bucket = ${ARCHIVE_URL}
+human_readable_sizes = True
+multipart_chunk_size_mb = 5
+use_https = True
+socket_timeout = 30
+EOF
+
+  if [ -n "${ARCHIVE_CACERT}" ]; then
+     echo "ca_certs_file = ${ARCHIVE_CACERT}" >> /tmp/s3cmd.cfg
+  fi
+  echo "Removing s3://${ARCHIVE_BUCKET}/$archivepath"
+  s3cmd  --region="${ARCHIVE_REGION}" -c /tmp/s3cmd.cfg del "s3://${ARCHIVE_BUCKET}/$archivepath"
+fi
+
+sleep 3000
+exit 0
+
