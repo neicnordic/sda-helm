0f4c2a09619f5950d25c9cc5209506d9a459fa6e
jonandernovella <novella.uu@gmail.com>
2021-01-13 15:46:35 +0100

    Put orch config values in file

--- dev_tools/scripts/deploy-sda-orch.sh
+++ dev_tools/scripts/deploy-sda-orch.sh
@@ -6,20 +6,8 @@ pushd sda-helm
 DB_IN_PASS=$(grep pg_in_password sda-deploy-init/config/trace.yml | awk {'print $2'} | sed --expression 's/\"//g')
 DB_OUT_PASS=$(grep pg_out_password sda-deploy-init/config/trace.yml | awk {'print $2'} | sed --expression 's/\"//g')
 
-helm install orch sda-orch \
---set podSecurityPolicy.create=false,\
-broker.password="admin",\
-broker.username=admin,\
-broker.host="broker-sda-mq",\
-broker.exchange="sda",\
-broker.vhost="sda",\
-broker.queue.inbox="inbox",\
-broker.queue.completed="completed",\
-broker.queue.verify="archived",\
-broker.queue.files="files",\
-broker.queue.stabledid="accessionIDs",\
-db.host="postgres-sda-db",\
-db.passIngest="$DB_IN_PASS",\
+helm install orch sda-orch -f .github/ci_tests/orch.yaml \
+--set db.passIngest="$DB_IN_PASS",\
 db.passOutgest="$DB_OUT_PASS"
 
 popd
