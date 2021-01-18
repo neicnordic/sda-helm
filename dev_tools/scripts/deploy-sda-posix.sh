ee83751381f5e362bb7d5f1f03f84334d286c8dd
Stefan Negru <stefan@blankdots.com>
2021-01-18 12:07:10 +0200

    fix SC2140

--- dev_tools/scripts/deploy-sda-posix.sh
+++ dev_tools/scripts/deploy-sda-posix.sh
@@ -7,7 +7,7 @@ DB_OUT_PASS=$(grep pg_out_password sda-deploy-init/config/trace.yml | awk '{prin
 C4GH_PASSPHRASE=$(grep ega_c4gh_passphrase sda-deploy-init/config/trace.yml | awk '{print $2}' | sed -e 's/\"//g')
 
 helm install sda sda-svc -f dev_tools/config/posix.yaml \
---set global.broker.vhost="/sda",\
+--set global.broker.vhost=/sda,\
 global.c4gh.passphrase="$C4GH_PASSPHRASE",\
 global.cega.password="$CEGA_USERS_PASS",\
 global.db.passIngest="$DB_IN_PASS",\
