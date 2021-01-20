9cbbb37e3c6079007f566df200f399370fda0af0
Pontus Freyhult <pontus_github@soua.net>
2021-01-20 08:50:20 +0100

    Script to run helm test for installed releases

---
+++ dev_tools/scripts/run-helm-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+helm list --short 2>/dev/null | while read -r release; do
+    echo "Testing $release"
+    helm test "$release"
+    r=$?
+
+    if [ "$r" -ne 0 ]; then
+	kubectl get pod -o name | while read -r "$pod"; do
+	    echo "All logs for $pod"
+	    kubectl logs --all-containers "$pod"
+	done
+	exit "$r"
+    fi
+done
+							  
