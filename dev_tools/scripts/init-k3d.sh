398251cf0744f9998db45a203d7ebdcd38f1102c
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2021-04-22 13:17:58 +0200

    Bump k3d version to get a newer k8s version

--- dev_tools/scripts/init-k3d.sh
+++ dev_tools/scripts/init-k3d.sh
@@ -2,7 +2,7 @@
 
 if ! command -v k3d > /dev/null
 then
-  wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v3.0.0 bash
+  wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v4.4.2 bash
 fi
 
 k3d cluster list | grep "sda"
@@ -10,7 +10,7 @@ cluster_exists=$?
 
 if [ $cluster_exists -ne 0 ]; then
   sudo k3d cluster create sda
-  sudo k3d kubeconfig merge sda --switch-context
+  sudo k3d kubeconfig merge sda --kubeconfig-switch-context
   sudo mkdir -p ~/.kube/ && sudo cp /root/.k3d/kubeconfig-sda.yaml ~/.kube/config
   sudo chmod 666 ~/.kube/config
 else
