2dec14575e9d29771185559d8abe6eedc94606a5
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2021-12-07 08:12:49 +0100

    test new wait for line

--- dev_tools/scripts/wait-for-pods.sh
+++ dev_tools/scripts/wait-for-pods.sh
@@ -8,7 +8,7 @@ if [ -n "$3" ]; then NAMESPACE=$3; else NAMESPACE="default"; fi
 for p in $SVCNAME
 do
   RETRY_TIMES=0
-  until kubectl get pods -n=$NAMESPACE -l=$LABEL=$p -o jsonpath='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status}' | grep "Ready=True"
+  until kubectl get pods -n=$NAMESPACE -l=$LABEL=$p -o jsonpath='{..status.containerStatuses[*].ready}' | grep "true"
   do
     echo "waiting for $p to become ready";
     RETRY_TIMES=$((RETRY_TIMES+1));
