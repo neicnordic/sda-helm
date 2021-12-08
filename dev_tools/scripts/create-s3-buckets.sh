0d8a11a87ca0bc6fb162fbcea1e84d4cfd9e1e9c
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2021-12-08 16:01:39 +0100

    [scripts] Fix issue when creating the s3conf
    indentation caused the config file to be invalid

--- dev_tools/scripts/create-s3-buckets.sh
+++ dev_tools/scripts/create-s3-buckets.sh
@@ -3,18 +3,18 @@ set -e
 
 if [ ! -f s3cmd.conf ]; then
   cat >> "s3cmd.conf" <<EOF
-  [default]
-  access_key=$(grep s3_access_key sda-deploy-init/config/trace.yml | awk '{print $2}' | sed -e 's/\"//g')
-  secret_key=$(grep s3_secret_key sda-deploy-init/config/trace.yml | awk '{print $2}' | sed -e 's/\"//g')
-  check_ssl_certificate = False
-  encoding = UTF-8
-  encrypt = False
-  guess_mime_type = True
-  host_base = https://localhost:9000
-  host_bucket = https://localhost:9000
-  use_https = True
-  socket_timeout = 30
-  ca_certs_file = sda-svc/files/ca.crt
+[default]
+access_key=$(grep s3_access_key sda-deploy-init/config/trace.yml | awk '{print $2}' | sed -e 's/\"//g')
+secret_key=$(grep s3_secret_key sda-deploy-init/config/trace.yml | awk '{print $2}' | sed -e 's/\"//g')
+check_ssl_certificate = False
+encoding = UTF-8
+encrypt = False
+guess_mime_type = True
+host_base = https://localhost:9000
+host_bucket = https://localhost:9000
+use_https = True
+socket_timeout = 30
+ca_certs_file = sda-svc/files/ca.crt
 EOF
 fi
 
