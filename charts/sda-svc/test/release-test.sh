f8bef3a4fde3a7e4ebb48ce487ed4b33a8d9b459
Stefan Negru <stefan@blankdots.com>
2021-12-10 23:57:17 +0200

    add sda download to s3 tests

--- charts/sda-svc/test/release-test.sh
+++ charts/sda-svc/test/release-test.sh
@@ -32,10 +32,7 @@ if [ "${DEPLOYMENT_TYPE}" = all -o "${DEPLOYMENT_TYPE}" = external ]; then
     echo "DOA seems okay"
   fi
 
-
-    if [ "${ARCHIVE_STORAGE_TYPE}" = posix ]; then
-      python3 /release-test-app/verify-download.py
-    fi
+    python3 /release-test-app/verify-download.py
 
     if [ "$?" -ne 0 ]; then
 	echo "Failed download verification, bailing out"
