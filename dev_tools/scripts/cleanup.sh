b8d79f5e92cb5f6d1807aabc6b69afda0c528f95
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2021-12-22 11:15:16 +0100

    [scripts] add cleanup script

---
+++ dev_tools/scripts/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$1" = "db" ] || [ "$1" = "all" ];then
+    helm uninstall postgres
+    kubectl delete secret db-certs
+fi
+
+if [ "$1" = "mq" ] || [ "$1" = "all" ];then
+    helm uninstall broker
+    kubectl delete secret mq-certs
+fi
+
+if [ "$1" = "sda" ] || [ "$1" = "all" ];then
+    helm uninstall sda
+    kubectl delete secret auth-certs backup-certs doa-certs finalize-certs inbox-certs ingest-certs intercept-certs mapper-certs tester-certs verify-certs ca-root
+fi
