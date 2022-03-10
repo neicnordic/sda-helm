cada95f48e3344d6d5b6ccea28b8c0c605f65999
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2022-03-10 10:40:34 +0100

    [test][no-tls] refactor

--- dev_tools/scripts/sda/deploy-no-tls.sh
+++ dev_tools/scripts/sda/deploy-no-tls.sh
@@ -6,17 +6,57 @@ if [ "$1" = "database" ]; then
     DB_OUT_PASS=$(grep pg_out_password sda-deploy-init/config/trace.yml | awk '{print $2}' | sed -e 's/\"//g')
 
     helm install postgres charts/sda-db \
-        --set securityPolicy.create=false,global.pg_in_password="$DB_IN_PASS",global.pg_out_password="$DB_OUT_PASS",global.tls.enabled=false
+        --set global.pg_in_password="$DB_IN_PASS",global.pg_out_password="$DB_OUT_PASS",securityPolicy.create=false,global.tls.enabled=false,persistence.enabled=false
+
+    RETRY_TIMES=0
+    until kubectl get pods -l role=database -o jsonpath='{..status.containerStatuses[*].ready}' | grep "true"; do
+        echo "waiting for database to become ready"
+        RETRY_TIMES=$((RETRY_TIMES + 1))
+        if [ "$RETRY_TIMES" -eq 30 ]; then
+            kubectl describe pod -l role=datbase
+            kubectl logs -l role=datbase
+            exit 1
+        fi
+        sleep 10
+    done
+    exit 0
 fi
 
 if [ "$1" = "broker" ]; then
+    HASH="$(/bin/sh dev_tools/scripts/mq-password-generator.sh admin)"
     helm install broker charts/sda-mq \
-        --set securityPolicy.create=false,config.adminUser=admin,config.adminPasswordHash=DI0kJIvQHptGSBH2coZ25dsjjN9Z4uxp8hAyqtd9H7rb/SBO,config.tls.enabled=false,config.vhost=sda
+        --set securityPolicy.create=false,global.adminUser=admin,global.adminPasswordHash="$HASH",global.tls.enabled=false,global.vhost=sda,persistence.enabled=false
+
+    RETRY_TIMES=0
+    until kubectl get pods -l role=broker -o jsonpath='{..status.containerStatuses[*].ready}' | grep "true"; do
+        echo "waiting for broker to become ready"
+        RETRY_TIMES=$((RETRY_TIMES + 1))
+        if [ "$RETRY_TIMES" -eq 30 ]; then
+            kubectl describe pod -l role=broker
+            kubectl logs -l role=broker
+            exit 1
+        fi
+        sleep 10
+    done
+    exit 0
 fi
 
 if [ "$1" = "orchestrate" ]; then
     helm install orch charts/sda-orch -f dev_tools/config/orch.yaml \
         --set tls.enabled=false,broker.port=5672,broker.queue.verify=verified,broker.queue.inbox=files
+
+    RETRY_TIMES=0
+    until kubectl get pods -l role=orchestrate -o jsonpath='{..status.containerStatuses[*].ready}' | grep "true"; do
+        echo "waiting for orch to become ready"
+        RETRY_TIMES=$((RETRY_TIMES + 1))
+        if [ "$RETRY_TIMES" -eq 30 ]; then
+            kubectl describe pod -l role=orchestrate
+            kubectl logs -l role=orchestrate
+            exit 1
+        fi
+        sleep 10
+    done
+    exit 0
 fi
 
 if [ "$1" = "minio" ]; then
@@ -29,6 +69,19 @@ if [ "$1" = "minio" ]; then
     helm install minio minio/minio \
         --set accessKey="$MINIO_ACCESS",secretKey="$MINIO_SECRET",tls.enabled=false,persistence.enabled=false,service.port=9000 \
         --version 8.0.8
+
+    RETRY_TIMES=0
+    until kubectl get pods -l app=minio -o jsonpath='{..status.containerStatuses[*].ready}' | grep "true"; do
+        echo "waiting for minio to become ready"
+        RETRY_TIMES=$((RETRY_TIMES + 1))
+        if [ "$RETRY_TIMES" -eq 30 ]; then
+            kubectl describe pod -l app=minio
+            kubectl logs -l app=minio
+            exit 1
+        fi
+        sleep 10
+    done
+    exit 0
 fi
 
 if [ "$1" = "pipeline" ]; then
--- dev_tools/scripts/wait-for-pods.sh
+++ dev_tools/scripts/wait-for-pods.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 set -e
 
-if [ -n "$1" ]; then SVCNAME=$1; else SVCNAME="svc"; fi
-if [ -n "$2" ]; then LABEL=$2; else LABEL="role"; fi
-if [ -n "$3" ]; then NAMESPACE=$3; else NAMESPACE="default"; fi
+base_list="backup download finalize inbox ingest mapper verify"
 
-for p in $SVCNAME
-do
+if [ -n "$1" ]; then
+  case "$1" in
+  federated_s3_svc_list)
+  SVCNAME="$base_list auth intercept"
+  ;;
+
+  federated_posix_svc_list)
+  SVCNAME="$base_list intercept"
+  ;;
+
+  standalone_s3_svc_list)
+  SVCNAME="$base_list auth"
+  ;;
+
+  standalone_posix_svc_list)
+  SVCNAME="$base_list"
+  ;;
+  esac
+fi
+
+if [ -n "$2" ]; then
+  LABEL=$2
+else
+  LABEL="role"
+fi
+if [ -n "$3" ]; then
+  NAMESPACE=${3:-default}
+fi
+
+for p in $SVCNAME; do
   RETRY_TIMES=0
-  until kubectl get pods -n=$NAMESPACE -l=$LABEL=$p -o jsonpath='{..status.containerStatuses[*].ready}' | grep "true"
-  do
-    echo "waiting for $p to become ready";
-    RETRY_TIMES=$((RETRY_TIMES+1));
-    if [ $RETRY_TIMES -eq 30 ]; then
-      kubectl describe pod -n=$NAMESPACE -l=$LABEL=$p
-      kubectl logs -n=$NAMESPACE -l=$LABEL=$p
-      exit 1;
+  until kubectl get pods -n "$NAMESPACE" -l "$LABEL=$p" -o jsonpath='{..status.containerStatuses[*].ready}' | grep "true"; do
+    echo "waiting for $p to become ready"
+    RETRY_TIMES=$((RETRY_TIMES + 1))
+    if [ "$RETRY_TIMES" -eq 30 ]; then
+      kubectl describe pod -n "$NAMESPACE" -l "$LABEL"="$p"
+      kubectl logs -n "$NAMESPACE" -l "$LABEL=$p"
+      exit 1
     fi
-    sleep 10;
+    sleep 10
   done
 done
