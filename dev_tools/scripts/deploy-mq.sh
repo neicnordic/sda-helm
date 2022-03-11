87dff12f5b8197e6250ff349634f918b2ae64aa7
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2022-03-11 14:44:39 +0100

    [sda-svc][tests] add certificate issuer to MQ deployment

--- dev_tools/scripts/deploy-mq.sh
+++ dev_tools/scripts/deploy-mq.sh
@@ -7,7 +7,11 @@ fi
 
 HASH="DI0kJIvQHptGSBH2coZ25dsjjN9Z4uxp8hAyqtd9H7rb/SBO"
 
-if [ -f "sda-deploy-init/config/certs/ca.crt" ]; then
+if [ "$2" = "issuer" ]; then
+    helm install broker charts/sda-mq \
+        --set securityPolicy.create=false,global.adminUser=admin,global.adminPasswordHash="$HASH",global.shovel.host=cega-mq,global.shovel.user=lega,global.shovel.pass="$CEGA_MQ_PASS",global.shovel.vhost=lega,global.vhost=sda,global.tls.issuer=ca-issuer,global.tls.verifyPeer=true
+else
+
     ## sda-mq certs
     kubectl create secret generic mq-certs \
         --from-file=sda-deploy-init/config/certs/ca.crt \
@@ -21,7 +25,4 @@ if [ -f "sda-deploy-init/config/certs/ca.crt" ]; then
 
     helm install broker charts/sda-mq \
         --set securityPolicy.create=false,global.adminUser=admin,global.adminPasswordHash="$HASH",global.shovel.host=cega-mq,global.shovel.user=lega,global.shovel.pass="$CEGA_MQ_PASS",global.shovel.vhost=lega,global.vhost=sda,global.tls.secretName=mq-certs,global.tls.keyName=server.key,global.tls.certName=server.crt,global.tls.caCert=ca.crt,global.tls.verifyPeer=true,testimage.tls.secretName=mq-tester-certs,testimage.tls.tlsKey=tester.key,testimage.tls.tlsCert=tester.crt,testimage.tls.caCert=ca.crt
-else
-    helm install broker charts/sda-mq \
-        --set securityPolicy.create=false,global.adminUser=admin,global.adminPasswordHash="$HASH",global.shovel.host=cega-mq,global.shovel.user=lega,global.shovel.pass="$CEGA_MQ_PASS",global.shovel.vhost=lega,global.vhost=sda,global.tls.secretName=mq-certs,global.tls.serverKey=server.key,global.tls.serverCert=server.crt,global.tls.caCert=ca.crt,global.tls.verifyPeer=true,testimage.tls.secretName=mq-certs,testimage.tls.tlsKey=tls.key,testimage.tls.tlsCert=tls.crt,testimage.tls.caCert=ca.crt
 fi
