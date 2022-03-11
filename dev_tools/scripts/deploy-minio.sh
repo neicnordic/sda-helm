3bc8013fdf16f26170a7e0f2035061eb1092c930
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2022-03-11 14:44:50 +0100

    [sda-svc][tests] add certificate issuer to Minio deployment

--- dev_tools/scripts/deploy-minio.sh
+++ dev_tools/scripts/deploy-minio.sh
@@ -4,12 +4,17 @@ set -e
 helm repo add minio https://helm.min.io/
 helm repo update
 
-kubectl create secret generic minio-certs --from-file=sda-deploy-init/config/certs/public.crt --from-file=sda-deploy-init/config/certs/private.key
-
 MINIO_ACCESS=$(grep s3_access_key sda-deploy-init/config/trace.yml | awk '{print $2}' | sed -e 's/\"//g')
 MINIO_SECRET=$(grep s3_secret_key sda-deploy-init/config/trace.yml | awk '{print $2}' | sed -e 's/\"//g')
 
-helm install minio minio/minio \
---set accessKey="$MINIO_ACCESS",secretKey="$MINIO_SECRET",\
-tls.enabled=true,tls.certSecret=minio-certs,\
-persistence.enabled=true,persistence.size=2Gi,service.port=443 --version 8.0.8
+if [ "$1" = "issuer" ]; then
+    kubectl apply -f dev_tools/config/minio-issuer.yaml
+
+    helm install minio minio/minio \
+        --set accessKey="$MINIO_ACCESS",secretKey="$MINIO_SECRET",tls.enabled=true,tls.certSecret=minio-certs,tls.publicCrt=tls.crt,tls.privateKey=tls.key,persistence.enabled=false,service.port=443 --version 8.0.8
+else
+    kubectl create secret generic minio-certs --from-file=sda-deploy-init/config/certs/public.crt --from-file=sda-deploy-init/config/certs/private.key
+
+    helm install minio minio/minio \
+        --set accessKey="$MINIO_ACCESS",secretKey="$MINIO_SECRET",tls.enabled=true,tls.certSecret=minio-certs,persistence.enabled=false,service.port=443 --version 8.0.8
+fi
